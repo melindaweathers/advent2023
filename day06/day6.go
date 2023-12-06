5c09e16f115714441191cc1d1d7ca52a7fbd6669
Melinda Weathers <melindaweathers@gmail.com>
2023-12-06 09:45:59 -0500

    Day 6 Part 1

---
+++ day06/day6.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func numWaysToBeatRecord(duration int, record int) int {
+	total := 0
+	for chargeTime := 0; chargeTime < duration; chargeTime++ {
+		distance := chargeTime * (duration - chargeTime)
+		if distance > record {
+			total++
+		}
+	}
+	return total
+}
+
+func toInt(s string) int {
+	num, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return num
+}
+
+func process(filename string, processor func(int, int) int) int {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(string(b), "\n")
+	times := strings.Fields(lines[0])
+	distances := strings.Fields(lines[1])
+	total := 1
+	for i := 1; i < len(times); i++ {
+		total *= processor(toInt(times[i]), toInt(distances[i]))
+	}
+
+	return total
+}
+
+func main() {
+	fmt.Println(process("./input-test.txt", numWaysToBeatRecord))
+	fmt.Println(process("./input.txt", numWaysToBeatRecord))
+}
