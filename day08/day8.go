08a4fbfc659814d03f918405215fbcd266c075be
Melinda Weathers <melindaweathers@gmail.com>
2023-12-09 11:45:22 -0500

    Day 8 Part 2

--- day08/day8.go
+++ day08/day8.go
@@ -8,13 +8,22 @@ import (
 )
 
 type Node struct {
-	left  string
-	right string
+	left     string
+	right    string
+	pathsToZ []Path
+}
+
+type Path struct {
+	distance int
+	start    string
+	stop     string
+	ptr      int
 }
 
 type Network struct {
-	nodes        map[string]Node
-	instructions string
+	nodes         map[string]Node
+	instructions  string
+	startingNodes []string
 }
 
 func readNetwork(filename string) Network {
@@ -22,7 +31,7 @@ func readNetwork(filename string) Network {
 	if err != nil {
 		log.Fatal(err)
 	}
-	network := Network{nodes: map[string]Node{}}
+	network := Network{nodes: map[string]Node{}, startingNodes: []string{}}
 	for i, line := range strings.Split(string(b), "\n") {
 		if i == 0 {
 			network.instructions = line
@@ -33,18 +42,26 @@ func readNetwork(filename string) Network {
 				panic(err)
 			}
 			// AAA = (BBB, CCC)
-			network.nodes[thisNode] = Node{left: leftNode[1 : len(leftNode)-1], right: rightNode[:len(rightNode)-1]}
+			network.nodes[thisNode] = Node{pathsToZ: []Path{}, left: leftNode[1 : len(leftNode)-1], right: rightNode[:len(rightNode)-1]}
+			if strings.HasSuffix(thisNode, "A") {
+				network.startingNodes = append(network.startingNodes, thisNode)
+			}
 		}
 	}
 	return network
 }
 
-func countSteps(filename string) int {
+func countSteps(network Network, start string, suffix string, startPtr int) (int, string) {
 	steps := 0
-	stepName := "AAA"
-	ptr := 0
-	network := readNetwork(filename)
-	for stepName != "ZZZ" {
+	stepName := start
+	ptr := startPtr
+	startNode := network.nodes[start]
+	for _, path := range startNode.pathsToZ {
+		if path.ptr == startPtr && path.start == start {
+			return path.distance, path.stop
+		}
+	}
+	for !strings.HasSuffix(stepName, suffix) {
 		dir := network.instructions[ptr]
 		if dir == 'L' {
 			stepName = network.nodes[stepName].left
@@ -56,10 +73,69 @@ func countSteps(filename string) int {
 		steps++
 		ptr = (ptr + 1) % len(network.instructions)
 	}
+	startNode.pathsToZ = append(startNode.pathsToZ, Path{ptr: startPtr, start: start, stop: stepName, distance: steps})
+	return steps, stepName
+}
+
+// https://siongui.github.io/2017/06/03/go-find-lcm-by-gcd/
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// https://siongui.github.io/2017/06/03/go-find-lcm-by-gcd/
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers []int) int {
+	result := a * b / GCD(a, b)
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i], []int{})
+	}
+	return result
+}
+
+func countStepsPart1(filename string) int {
+	network := readNetwork(filename)
+	steps, _ := countSteps(network, "AAA", "ZZZ", 0)
+	return steps
+}
+
+func allDone(nodes []string) bool {
+	for _, node := range nodes {
+		if !strings.HasSuffix(node, "Z") {
+			return false
+		}
+	}
+	return true
+}
+
+func countGhostSteps(filename string) int {
+	steps := 0
+	ptr := 0
+	network := readNetwork(filename)
+	currentNodes := network.startingNodes
+	for !allDone(currentNodes) {
+		stepSize := 1
+		nextNodes := []string{}
+		for _, node := range currentNodes {
+			stepsToZ, nodeWithZ := countSteps(network, node, "Z", ptr)
+			stepSize = LCM(stepSize, stepsToZ, []int{})
+			nextNodes = append(nextNodes, nodeWithZ)
+		}
+		steps += stepSize
+		ptr = (ptr + stepSize) % len(network.instructions)
+		currentNodes = nextNodes
+	}
 	return steps
 }
 
 func main() {
-	fmt.Println(countSteps("./input-test.txt"))
-	fmt.Println(countSteps("./input.txt"))
+	fmt.Println(countStepsPart1("./input-test.txt"))
+	fmt.Println(countStepsPart1("./input.txt"))
+	fmt.Println(countGhostSteps("./input-test.txt"))
+	fmt.Println(countGhostSteps("./input.txt"))
 }
