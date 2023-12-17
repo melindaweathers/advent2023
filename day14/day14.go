24566f3bb2a383962990d9cea683dd7e13a0bf42
Melinda Weathers <melindaweathers@gmail.com>
2023-12-17 17:01:21 -0500

    Day 14 part 2

--- day14/day14.go
+++ day14/day14.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -31,6 +32,17 @@ func shiftNorth(lines []string) ([]string, int) {
 	return shiftedLines, numShifted
 }
 
+func rotate(lines []string) []string {
+	rotated := make([]string, len(lines[0]))
+	for i := len(lines) - 1; i >= 0; i-- {
+		for j := 0; j < len(lines[0]); j++ {
+			rotated[j] = rotated[j] + string(lines[i][j])
+		}
+	}
+
+	return rotated
+}
+
 func countLoad(lines []string) int {
 	load := 0
 	for i, line := range lines {
@@ -44,6 +56,16 @@ func countLoad(lines []string) int {
 	return load
 }
 
+func fullyShiftNorth(lines []string) ([]string, int) {
+	numShifted := 1
+	total := 0
+	for numShifted > 0 {
+		lines, numShifted = shiftNorth(lines)
+		total += numShifted
+	}
+	return lines, total
+}
+
 func northLoad(filename string) int {
 	b, err := os.ReadFile(filename)
 	if err != nil {
@@ -51,15 +73,43 @@ func northLoad(filename string) int {
 	}
 
 	lines := strings.Split(string(b), "\n")
-	numShifted := 1
-	for numShifted > 0 {
-		lines, numShifted = shiftNorth(lines)
+	lines, _ = fullyShiftNorth(lines)
+	return countLoad(lines)
+}
+
+func fullSpin(filename string, numTimes int) int {
+	b, err := os.ReadFile(filename)
+	history := []string{}
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	return countLoad(lines)
+	lines := strings.Split(string(b), "\n")
+	loopAt := -1
+	singleString := ""
+	for times := 0; times < numTimes && loopAt < 0; times++ {
+		for dir := 0; dir < 4; dir++ {
+			lines, _ = fullyShiftNorth(lines)
+			lines = rotate(lines)
+		}
+		singleString = strings.Join(lines, "~")
+		loopAt = slices.Index(history, singleString)
+
+		// fmt.Println(countLoad(lines))
+		if loopAt < 0 {
+			history = append(history, singleString)
+		}
+	}
+
+	loopSize := len(history) - loopAt
+	index := loopAt + ((numTimes - 1 - loopAt) % loopSize)
+
+	return countLoad(strings.Split(history[index], "~"))
 }
 
 func main() {
 	fmt.Println(northLoad("./input-test.txt"))
 	fmt.Println(northLoad("./input.txt"))
+	fmt.Println(fullSpin("./input-test.txt", 1000000000))
+	fmt.Println(fullSpin("./input.txt", 1000000000))
 }
